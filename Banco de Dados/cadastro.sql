1a12c12be57d19aa04af49c9315c3a8d3f688192
Jonas-Grossi <jonasgrossicc@gmail.com>
2020-05-18 14:32:45 -0300

    Jonas Grossi-Banco de dados atualizado
    banco de dados importado!!

---
+++ Banco de Dados/cadastro.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18-Maio-2020 às 19:31
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `cadastro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `nome` varchar(250) NOT NULL,
+  `sobrenome` varchar(250) NOT NULL,
+  `pais` varchar(250) NOT NULL,
+  `estado` varchar(250) NOT NULL,
+  `cidade` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `senha` varchar(250) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`nome`, `sobrenome`, `pais`, `estado`, `cidade`, `email`, `senha`, `id`) VALUES
+('jonas', 'grossi', 'brasil', 'mg', 'silveirania', 'jonasgrossicc@gmail.com', '123', 1),
+('Jonas123', 'Grossi', 'Brasil', 'MG', 'Silveirania', 'jonasgrossicc@gmail.com', '1', 5),
+('teste', 'teste', 'uruguai', 'pr', 'tototot', 'jonasgrossi@yahoo.com.br', '123', 4),
+('', '', '', '', '', '', '', 6),
+('jose', 'grossi', 'brasil', 'mg', 'silveirania', 'negogrossi@yahoo.com.br', '123', 7),
+('jose', 'grossi', 'brasil', 'mg', 'silveirania', 'negogrossi@yahoo.com.br', '123', 8),
+('teste', 'teste1', 'brasil', 'mg', 'Silveirania', 'jonasgrossi@yahoo.com.br', '1', 9),
+('nego', 'grossi', 'brasil', 'mg', 'silveirania', 'negogrossi@yahoo.com.br', '123', 10);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
